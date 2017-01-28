e185fac6e3ef1d6d4bb750da982102859d85a235
Michael Lenover <mlenover@gmail.com>
2017-01-28 15:41:59 -0500

    Resolved accelerometer conflicts

--- TrackingAntennaV3/main/Accelerometer.cpp
+++ TrackingAntennaV3/main/Accelerometer.cpp
@@ -10,17 +10,13 @@
 #include <Math.h>
 
 //Create accelerometer object
-static Adafruit_LSM303_Accel_Unified accel;
+static Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Mag_Unified(COMPASS_ID);
 
 //Declare accelerometer event variable
 static sensors_event_t accelEvent;
 
 //Initializes accelerometer struct
-<<<<<<< HEAD
 extern volatile AccelerometerData accelerometerData;
-=======
-extern volatile Accelerometer accelerometer;
->>>>>>> origin/Accelerometer
 
 //Initializes accelerometer
 bool initAccelerometer(){
@@ -30,32 +26,18 @@ bool initAccelerometer(){
  //Initializes accelerometer
   if(!accel.begin()){
     error("\nAccelerometer failed to be detected.");
-<<<<<<< HEAD
     return 0;
-=======
->>>>>>> origin/Accelerometer
  }
-
  return 1;
 }
 
 //Retrieves accelerometer data
-<<<<<<< HEAD
 void getGravity(){
-=======
-void getGravity(float *pNegGravity){
->>>>>>> origin/Accelerometer
   //Gets accelerometer event data
   accel.getEvent(&accelEvent);
  
   //Assigns data to accelerometer struct variables
-<<<<<<< HEAD
   accelerometerData.x = accelEvent.acceleration.x;
   accelerometerData.y = accelEvent.acceleration.y;
   accelerometerData.z = accelEvent.acceleration.z;
-=======
-  accelerometer.x = accelEvent.acceleration.x;
-  accelerometer.y = accelEvent.acceleration.y;
-  accelerometer.z = accelEvent.acceleration.z;
->>>>>>> origin/Accelerometer
 }
