9b8f7c0a8a95b13e6a71cc492ec423dd1d3aafc8
Michael Lenover <mlenover@gmail.com>
2017-01-28 15:15:10 -0500

    Accelerometer Comments, Modifications
    Added additional info in header comment. Changed accel intialization
    function to boolean. Removed unnecassary input variable in getGravity().

--- TrackingAntennaV3/main/Accelerometer.cpp
+++ TrackingAntennaV3/main/Accelerometer.cpp
@@ -7,6 +7,7 @@
 #include "Logger.hpp"
 #include "Accelerometer.hpp"
 #include <Adafruit_LSM303_U.h>
+#include <Math.h>
 
 //Create accelerometer object
 static Adafruit_LSM303_Accel_Unified accel;
@@ -15,27 +16,29 @@ static Adafruit_LSM303_Accel_Unified accel;
 static sensors_event_t accelEvent;
 
 //Initializes accelerometer struct
-volatile Accelerometer accelerometer;
+extern volatile AccelerometerData accelerometerData;
 
 //Initializes accelerometer
-void initAccelerometer(){
+bool initAccelerometer(){
  //Defines accel object based on compass id
  accel = Adafruit_LSM303_Accel_Unified(COMPASS_ID);
 
  //Initializes accelerometer
- if(!accel.begin()){
-   error("\nAccelerometer failed to be detected.");
+  if(!accel.begin()){
+    error("\nAccelerometer failed to be detected.");
+    return 0;
  }
+
+ return 1;
 }
 
 //Retrieves accelerometer data
 void getGravity(){
- //Gets accelerometer event data
- accel.getEvent(&accelEvent);
+  //Gets accelerometer event data
+  accel.getEvent(&accelEvent);
  
- //Assigns data to accelerometer struct variables
- accelerometer.x = accelEvent.acceleration.x;
- accelerometer.y = accelEvent.acceleration.y;
- accelerometer.z = accelEvent.acceleration.z;
+  //Assigns data to accelerometer struct variables
+  accelerometerData.x = accelEvent.acceleration.x;
+  accelerometerData.y = accelEvent.acceleration.y;
+  accelerometerData.z = accelEvent.acceleration.z;
 }
-
--- TrackingAntennaV3/main/Accelerometer.hpp
+++ TrackingAntennaV3/main/Accelerometer.hpp
@@ -2,22 +2,35 @@
  * @file   Magnetometer.hpp
  * @Author Michael Lenover (WARG)
  * @date   January 25, 2017
- * @brief  
- * @see https://www.arduino.cc/en/Reference/Ethernet
+ * @brief  Accelerometer initialization and data aquisition functions.
+ * Uses Adafruit LSM303 Unified Acceleromter/Magnetometer for accelerometer data.
+ * Connects to Arduino using I2C communication
+ * Connect pin SDA on accelerometer to pin 20
+ * Connect pin SCL on accelerometer to pin 21
+ * Connect power to 3.3V, ground to ground
+ * @see https://www.arduino.cc/en/Reference/Wire
+ * @see https://www.adafruit.com/product/1120
  */
 
 #ifndef ACCELEROMETER
 #define ACCELEROMETER
 
+//Arbirtrary ID used to identify sensor if additional sensors are added
 #define COMPASS_ID 12345
 
-//Accelerometer data struct
+/**
+ * Accelerometer data struct 
+ * Contains x,y,z components of vector pointing in direction of gravity
+ */
 typedef struct {
   float x;
   float y;
   float z;
-} Accelerometer;
+} AccelerometerData;
 
 void initAccelerometer();
 
+void getGravity();
+
 #endif
+
